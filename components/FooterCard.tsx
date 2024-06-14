356dfd59d86de5d64554b7e287fc77534e834d4a
kartikver15gr8 <vermakartikey786@gmail.com>
2024-06-15 00:35:37 +0530

    modified FooterCard

--- components/FooterCard.tsx
+++ components/FooterCard.tsx
@@ -12,7 +12,7 @@ export default function FooterCard({
   l4?: string;
 }) {
   return (
-    <div className="flex flex-col gap-y-[10px] sm:gap-y-[10px] md:gap-y-[20px] lg:gap-y-[20px] xl:gap-y-[20px] 2xl:gap-y-[20px] mx-5 sm:mx-5 md:mx-5 lg:mx-10 xl:mx-10 2xl:mx-10">
+    <div className="flex flex-col gap-y-[10px] sm:gap-y-[10px] md:gap-y-[20px] lg:gap-y-[20px] xl:gap-y-[20px] 2xl:gap-y-[20px] mx-1 sm:mx-2 md:mx-5 lg:mx-10 xl:mx-10 2xl:mx-10">
       <p className="text-[12px] sm:text-[12px] md:text-[14px] lg:text-[16px] xl:text-[16px] 2xl:text-[16px] font-bold">
         {heading}
       </p>
